b5e6361c3534678a035998a65e10369dae448ca1
Naveen Kumar <naveen.thumoji66@gmail.com>
2025-12-29 21:19:17 +0530

    parent child components

--- force-app/main/default/lwc/parentComponentUsingAPI/parentComponentUsingAPI.js
+++ force-app/main/default/lwc/parentComponentUsingAPI/parentComponentUsingAPI.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class ParentComponentUsingAPI extends LightningElement {}
+export default class ParentComponentUsingAPI extends LightningElement {
+    message;
+    changeHandler(event){
+        this.message = event.target.value;
+   
+    }
+}

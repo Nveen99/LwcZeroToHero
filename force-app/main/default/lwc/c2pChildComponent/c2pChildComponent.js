cc7cff9e63bf15259868f22507ae218aa89b9962
Naveen Kumar <naveen.thumoji66@gmail.com>
2026-01-04 19:51:33 +0530

    child to parent communication

---
+++ force-app/main/default/lwc/c2pChildComponent/c2pChildComponent.js
@@ -0,0 +1,35 @@
+import { LightningElement } from 'lwc';
+
+export default class C2pChildComponent extends LightningElement {
+    handleClickAddition() {
+        const event = new CustomEvent('addition', {
+            detail: 'Addition'
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleClickSubtraction() {
+         const event1 = new CustomEvent('substraction', {
+            detail: 'substraction'
+        });
+        this.dispatchEvent(event1);
+    }
+
+
+    handleClickMul() {
+        const event2 = new CustomEvent('multiple', {
+            detail: 2
+        });
+        this.dispatchEvent(event2);
+    }
+    handleClickMultiply(event){
+        const factor = event.target.dataset.factor;
+        const event3 = new CustomEvent('multifactor', {
+            detail: factor
+        });
+        this.dispatchEvent(event3);
+
+    }
+
+    array=[3,2,4,6,0];
+}
---
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+
+export default class C2pParentComponent extends LightningElement {
+    number=0;
+    handleAddition(event){
+        if(this.number<1000){
+            this.number=this.number+1;
+        }
+        
+    }
+    
+    handleSubstraction(event){
+        if(this.number>0){
+     this.number=this.number-1;
+        }
+    }
+
+    handleMultiple(event){
+        this.number=this.number*event.detail;
+    }
+
+    handleMuliFactor(event){
+        this.number=this.number*event.detail;
+    }
+    
+} 
+
+
+
+
+
+
+
+
+
+
+
